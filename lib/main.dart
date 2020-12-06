16ac1a5de930c5e91fb5b907ade7e98aee9e9c02
Toe Wai Lin <toewailinbdl@gmail.com>
2020-12-06 21:48:04 +0630

    Add many animate direction

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,11 @@ Route _createRoute() {
   return PageRouteBuilder(
       pageBuilder: (context, animation, secondaryAnimation) => Page2(),
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        var begin = Offset(0.0, 1.0);
+        //var begin = Offset(0.0, 0.0); //(dx, dy) (horizon, vertical) //No animation
+        //var begin = Offset(0.0, 1.0); // From down to up
+        var begin = Offset(1.0, 0.0); // From right to left
+        //var begin = Offset(1.0, 1.0); // From bottom right corner
+        //var begin = Offset.fromDirection(-0.4, 0.2); //(direction, distance)
         var end = Offset.zero;
         var curve = Curves.ease;
         var tween =
